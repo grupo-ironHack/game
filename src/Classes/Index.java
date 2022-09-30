28a6c77f3d87598c1783821ae5c019f6e7423deb
Pol Bermudez <begu.43008@gmail.com>
2022-09-30 18:29:03 +0200

    final

--- src/Classes/Index.java
+++ src/Classes/Index.java
@@ -15,23 +15,23 @@ public class Index {
     //ARRAY LIST:
 //    ArrayList<String> myArrayList = new ArrayList<String>();
     public int numElements = 3;
-    String[] myDataArray = new String[]{"[1] CREATE & PERSONALIZE TEAMS", "[2] CREATE RANDOM TEAMS", "[0] EXIT" };
+    String[] myDataArray = new String[]{"[1] CREATE & PERSONALIZE TEAMS", "[2] CREATE RANDOM TEAMS", "[0] EXIT"};
     static String gameName = "RPG GAME";
 
     //VARIABLES PARA HACER FUNCIONAR INDICE (PRUEBAS)
     int option;
     public int hp = 100;
 
-    String result ="";
+    String result = "";
 
     boolean introSelector = false;
 
     //CONSTRUCTOR CARGAR EL MENU DE ACCIONES EN UN ARRAY:
-    public boolean intro(){
+    public boolean intro() {
         System.out.println("[1] Start Game\n[0] Exit");
         System.out.println("Select an option");
 
-        if(startIndex.nextInt() == 1) {
+        if (startIndex.nextInt() == 1) {
             introSelector = true;
         } else {
             System.out.println("Good Bye");
@@ -42,12 +42,12 @@ public class Index {
     }
 
     //MUESTRA LAS DIFERENTES OPCIONES PARA LAS ACCIONES:
-    public int options(){
+    public int options() {
         System.out.println(gameName);
 
         String strOptions = "";
-        for(int i=0; i<myDataArray.length; i++){
-            strOptions += myDataArray[i]+"\n";
+        for (int i = 0; i < myDataArray.length; i++) {
+            strOptions += myDataArray[i] + "\n";
         }
         //System.out.println(strLine);
         System.out.println(" - POSIBLE ACTIONS -");
@@ -58,5 +58,4 @@ public class Index {
 
         return option;
     }
-
 }
